0e1923ba609cd424850d5f7ef985a31c2ff50602
nicolas.mmb <nicolas.mmb@hotmail.com>
2024-02-13 11:57:00 -0300

    fuuuck

--- config/init.v2.sql
+++ config/init.v2.sql
@@ -20,22 +20,6 @@ BEGIN
 END;
 $$; 
 
--- Map (
---     "id" -> 1, "limite" -> 1000 * 100
--- ),
--- Map (
---     "id" -> 2, "limite" -> 800 * 100
--- ),
--- Map (
---     "id" -> 3, "limite" -> 10000 * 100
--- ),
--- Map (
---     "id" -> 4, "limite" -> 100000 * 100
--- ),
--- Map (
---     "id" -> 5, "limite" -> 5000 * 100
--- ),
-
 CREATE INDEX idx_compound_cliente_id_realizado_em ON transacoes (cliente_id, realizada_em);
 
 CREATE INDEX idx_transacoes_cliente_id ON transacoes (cliente_id);
